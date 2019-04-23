d11cde0aab5b34030c5c5020477ca5d89863f45e
rsakhi <sakhi.rohit@gmail.com>
2019-04-23 18:25:40 +0530

    project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'orgnaisation-detail',
+    pathMatch: 'full'
+  },
+
+  {
+    path: "organization-detail",
+    component: OrgnaisationComponenent
+  },
+  {
+    path: "analytics",
+    component: AnalyticsComponent
+  }
+];
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
